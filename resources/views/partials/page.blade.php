82ca02cb9040de13ab5be183375948f29b52c50a
Jonny Moon <git@jonnymoon.com>
2016-12-08 07:00:00 -0700

    Fixed showing of pages

--- resources/views/partials/page.blade.php
+++ resources/views/partials/page.blade.php
@@ -1,4 +1,6 @@
 <a class="page" href="{{ URL::route('pages.show', $page->id) }}" draggable="true" ondragstart="app.onPageDragStart(event, {{$page->id}})">{{ $page->name }}
-	<img style="width:100%;" src="{{ URL::asset('images/pages/thumbs/' . $page->colored_url) }}" />
-	<img style="width:100%;" src="{{ URL::asset('images/pages/thumbs/' . $page->outline_url) }}" />
+	@if($page->colored_url) 
+		<img src="{{ URL::asset('images/pages/thumbs/' . $page->colored_url) }}" />
+	@endif
+	<img src="{{ URL::asset('images/pages/thumbs/' . $page->outline_url) }}" />
 </a>
